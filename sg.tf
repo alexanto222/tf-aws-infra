8b41929f745d9cb5b7244e0fd7bf076960fdcbfc
root <root@ip-172-31-81-149.ec2.internal>
2020-03-28 16:44:34 +0000

    ELB added with corrections

--- sg.tf
+++ sg.tf
@@ -23,3 +23,26 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
+# ELB SG
+resource "aws_security_group" "elb" {
+  name        = "elb_sg"
+  description = "Used in the terraform"
+
+  vpc_id = aws_vpc.tf_vpc.id
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["125.16.91.5/32"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
